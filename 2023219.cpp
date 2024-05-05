2ada41735c45d8827afdbe57a09ff8c79c9079f4
Hamza-Bin-Aamir <148682978+Hamza-Bin-Aamir@users.noreply.github.com>
2024-05-05 20:22:28 +0500

    add boilerplate

---
+++ 2023219.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // contains the standard cin and cout implementations
+#include <exception> // need this for standard exceptions
+
+using namespace std; // so we dont have to repeatedly write std::cin, std::cout
+#define NO_ERRORS 0
+
+int main()
+{
+	
+
+	return NO_ERRORS; // this macro term "NO ERRORS" helps us clarify code
+}
