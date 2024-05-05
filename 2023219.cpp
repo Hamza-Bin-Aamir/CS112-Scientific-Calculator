f438cce9f20afea25b757afe5e23aea57e9d9906
Hamza-Bin-Aamir <148682978+Hamza-Bin-Aamir@users.noreply.github.com>
2024-05-05 20:30:34 +0500

    Create skeleton for try catch handling

--- 2023219.cpp
+++ 2023219.cpp
@@ -2,11 +2,40 @@
 #include <exception> // need this for standard exceptions
 
 using namespace std; // so we dont have to repeatedly write std::cin, std::cout
-#define NO_ERRORS 0
+
+enum ErrorCodes{
+	NO_ERRORS = 0,
+	UNKNOWN_ERR
+};
+
+string Calculate(string IN){
+
+}
 
 int main()
 {
-	
+	ErrorCodes ErrorCode;
+	string UserPrompt;
+	while(true){
+		UserPrompt = ""; // Good practice to reset the variable at every iteration
+		cin.clear(); // clears any errors in the input stream, as we will handle erroneous behavior 
 
-	return NO_ERRORS; // this macro term "NO ERRORS" helps us clarify code
+		cout << "Please enter your calculation. Write 'Exit' to Exit:";
+		cin >> UserPrompt;
+
+		if(UserPrompt == "Exit"){
+			return ErrorCode;
+		}
+
+		try {
+			Calculate(UserPrompt);
+		} 
+		
+		catch(...){
+			ErrorCode = UNKNOWN_ERR;
+			cout << "Unknown error while processing your calculation, please try again.";
+		}
+	}
+
+	return ErrorCode; // this macro term "NO ERRORS" helps us clarify code
 }
